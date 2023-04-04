678015bc0d314f9279a7ff0fda9d00c9ce9565f9
GUILLEM TURRÓ <gturro97@gmail.com>
2023-04-04 16:03:23 +0200

    First interlocutor form layout
    Changed base variables from bootstrap scss: added a new variable named '-focus-border-width' default is set to 1px.

--- resources/js/components/carta/form/InterlocutorForm.vue
+++ resources/js/components/carta/form/InterlocutorForm.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <div class="row g-3 align-items-center">
+        <div class="row align-items-center">
             <div class="col-2">
                 <label for="telef" class="col-form-label">Telèfon</label>
             </div>
@@ -8,7 +8,7 @@
                 <input type="phone" id="telef" class="form-control" aria-describedby="telefInterlocutor">
             </div>
         </div>
-        <div class="row g-3 align-items-center">
+        <div class="row align-items-center">
             <div class="col-2">
                 <label for="nom" class="col-form-label">Nom</label>
             </div>
@@ -16,6 +16,28 @@
                 <input type="text" id="nom" class="form-control" aria-describedby="nomInterlocutor">
             </div>
         </div>
+        <div class="row align-items-center">
+            <div class="col-2">
+                <label for="cognom" class="col-form-label">Cognom</label>
+            </div>
+            <div class="col-auto">
+                <input type="text" id="cognom" class="form-control" aria-describedby="cognomInterlocutor">
+            </div>
+        </div>
+
+        <div class="row align-items-center">
+            <label for="floatingTextarea2" class="form-label">Antecedents</label>
+            <textarea class="form-control" id="floatingTextarea2" style="height: 100px"></textarea>
+        </div>
+
+        <div class="form-check form-switch form-check-reverse">
+            <label class="form-check-label" for="saveInterlocutor">Guardar interlocutor</label>
+            <input class="form-check-input" type="checkbox" role="switch" id="saveInterlocutor">
+        </div>
+
+
+
+    
     </form>
 </template>
 <script>
@@ -24,6 +46,6 @@ export default {
 </script>
 <style scoped>
     form {
-        max-width: 1000px;
+        max-width: 850px;
     }
 </style>
